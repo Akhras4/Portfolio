6b0ea3f67c6d9ccd85f2fe92ce6e4b19adfa2996
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-07-23 19:46:37 +0200

    create  logo

---
+++ protfoilo/src/components/navbar/navbar.jsx
@@ -0,0 +1,10 @@
+import { useState,useEffect } from "react";
+import React from 'react'
+import Logo from "./logo/logo";
+export default function Navbar() {
+  return (
+    <div>
+        <Logo />
+    </div>
+  )
+}
